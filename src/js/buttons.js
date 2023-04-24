46fd318370f62a6ab57244e7257e738a0196bf22
Микола Іванишин <43409421+Mikaeri@users.noreply.github.com>
2023-04-24 22:51:26 +0100

    async/await

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -2,42 +2,50 @@ import refs from './refs.js';
 import { getImages } from './getImages.js';
 import { renderImageContainer } from './renderImageContainer.js';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { slowScrolling } from './slowScrolling.js';
+
 let page = 1;
 let lightBox = new SimpleLightbox('.photo-card-link', {
   captions: true,
   captionsData: 'alt',
   captionDelay: 250,
 });
-export function onSubmit(evt) {
+
+export async function onSubmit(evt) {
   evt.preventDefault();
   page = 1;
   refs.gallery.innerHTML = '';
 
-  getImages().then(resp => {
+  try {
+    const resp = await getImages();
     if (resp.data.total === 0) {
       Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
-    }
-    Notify.success(`Hooray! We found ${resp.data.totalHits} images.`);
-    refs.gallery.insertAdjacentHTML(
-      'beforeend',
-      renderImageContainer(resp.data.hits)
-    );
+    } else {
+      Notify.success(`Hooray! We found ${resp.data.totalHits} images.`);
+      refs.gallery.insertAdjacentHTML(
+        'beforeend',
+        renderImageContainer(resp.data.hits)
+      );
 
-    refs.loadMoreBtn.style.display = 'block';
-    lightBox.refresh();
-  });
+      refs.loadMoreBtn.style.display = 'block';
+      lightBox.refresh();
+      slowScrolling();
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-export function onLoadMoreClick(evt) {
+export async function onLoadMoreClick(evt) {
   evt.preventDefault();
   page += 1;
 
-  getImages(page).then(resp => {
+  try {
+    const resp = await getImages(page);
     if (page * 40 >= resp.data.totalHits) {
       refs.loadMoreBtn.style.display = 'none';
       Notify.info("We're sorry, but you've reached the end of search results.");
@@ -47,5 +55,9 @@ export function onLoadMoreClick(evt) {
       renderImageContainer(resp.data.hits)
     );
     lightBox.refresh();
-  });
+    slowScrolling();
+  } catch (error) {
+    console.error(error);
+    Notify.failure('Failed to load more images. Please try again later.');
+  }
 }
